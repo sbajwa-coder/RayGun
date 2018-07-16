edf61226ac3ae03799c14b66f86b44b801af6d71
Boolioos <juliusbarboza96@gmail.com>
2018-07-16 13:33:50 -0400

    finished camera

--- demos/demo_camera_only/js/game.js
+++ demos/demo_camera_only/js/game.js
@@ -29,20 +29,19 @@ gameScene.init = function(){
 gameScene.preload = function(){
 	//this.load.multiatlas('guy','assets/walking.json','assets'); <- loading atlas
 	this.load.atlas('zombie','assets/spritesheet.png', 'assets/spritesheet.json');
-
+	this.load.image('background','assets/map.png');
 	this.load.json('shape','assets/zombieShape.json');
 }
 
 gameScene.create = function(){
 	var box = this.cache.json.get('shape');
 	
+	//Player stuff
 	this.player = this.matter.add.sprite(150,250,'zombie',
 		'tile015.png',{shape:box.tile015});
 	this.player.setScale(2,2);
 	this.player.speed = 5;
 
-	//prefix (naming convention) for atlax
-	//this.anims.create({ key: 'jellyfish', frames: this.anims.generateFrameNames('sea', { prefix: 'blueJellyfish', end: 32, zeroPad: 4 }), repeat: -1 });
 	this.anims.create({
 		key: 'walk',
 		frames: this.anims.generateFrameNames('zombie', {
@@ -54,62 +53,42 @@ gameScene.create = function(){
 		repeat:-1,
 		//onUpdate: animUpdateCallback
 	});
-
-	this.anims.create({
-		key: 'attack',
-		frames: this.anims.generateFrameNames('zombie', {
-			frames: ['tile025.png','tile026.png','tile027.png',
-					'tile030.png','tile031.png','tile032.png',
-					'tile033png','tile034.png', 'tile015.png']
-		}),
-		frameRate: 15,
-		repeat:-1,
-		//onUpdate: animUpdateCallback
-	});
-	this.player.anims.play('walk');
-
-	this.player.on('animationupdate',animUpdate,this);
-
-	this.attack = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
 	cursor = this.input.keyboard.createCursorKeys();
-}
 
-function animUpdate(animation,frame){
-	box = this.cache.json.get('shape');
-	currFrame = frame.textureFrame.split('.')[0];
-	this.player.setBody(box[currFrame]);
-	this.player.setScale(2,2);
-}
-function animUpdateCallback ()
-{
-    console.log(1);
+	let bg = this.add.image(0,0,'background').setOrigin(0,0).setDepth(-1);
+	this.matter.world.setBounds(0,0,bg.width,bg.height);
+	console.log(bg);
+	this.cameras.main.startFollow(this.player);
+	this.cameras.main.setBounds(0,0,bg.width,bg.height);
+		
 }
 
 
 gameScene.update = function(){
-	if (this.attack.isDown){
-		this.player.anims.play('attack',true);
-	}
 	//Player movement update
 	if (cursor.left.isDown){
 		this.player.x -= (this.player.speed);
+		this.player.rotation = (90*(Math.PI/180))
 		this.player.anims.play('walk',true);
 		//this.player.x -= this.playerspeed;
 	}
 	else if (cursor.right.isDown){	
 		this.player.x += (this.player.speed);
 		this.player.anims.play('walk',true);
+		this.player.rotation = (-90*(Math.PI/180))
 		//this.player.x += this.playerspeed;
 	}
 
 	if (cursor.up.isDown){
 		this.player.y -= (this.player.speed);
 		this.player.anims.play('walk',true);
+		this.player.rotation = (180*(Math.PI/180))
 		//this.player.y -= this.playerspeed;
 	}
 	else if (cursor.down.isDown){
 		//this.player.y += this.playerspeed;
 		this.player.y += (this.player.speed);
+		this.player.rotation = (0*(Math.PI/180))
 		this.player.anims.play('walk',true);
 	}
 }
