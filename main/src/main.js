495f63b970404022d44a733019419db36347f7a8
Boolioos <juliusbarboza96@gmail.com>
2018-07-17 13:39:03 -0400

    setup files

--- main/src/main.js
+++ main/src/main.js
@@ -2,5 +2,25 @@ import 'phaser';
 import BootScene from './scenes/BootScene';
 
 const config = {
-	
-}
+	type: Phaser.AUTO,
+	height:600,
+	width:800,
+	physics: {
+		default: 'matter',
+		matter: {
+			debug:true,
+			setBounds: {
+				width: 800,
+				height: 600
+			},
+			gravity: {
+				y:0
+			}
+		}
+	},
+	scene:[
+		BootScene,
+	]	
+};
+
+const game = new Phaser.Game(config);
---
+++ main/src/scenes/BootScene.js
@@ -0,0 +1,13 @@
+class BootScene extends Phaser.Scene{
+	constructor(test){
+		super({
+			key: 'BootScene'
+		});
+	}
+
+	preload(){}
+
+	create(){}
+}
+
+export default BootScene;
