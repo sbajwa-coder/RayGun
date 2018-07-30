dc76d1ebd8e54f38e95f9ecdfec75ccb6f6ff5fb
Boolioos <juliusbarboza96@gmail.com>
2018-07-30 13:47:20 -0400

    some more map stufF

--- demos/demo_map/js/game.js
+++ demos/demo_map/js/game.js
@@ -2,10 +2,24 @@ let gameScene = new Phaser.Scene('game');
 
 let config = {
 	type: Phaser.AUTO,
-	width: 600,
-	height: 800 ,
-	scene: gameScene
+	height: 600,
+	width:800,
+	scene: gameScene,
+	physics: {
+		default: 'matter',
+		matter: {
+			debug:true,
+			setBounds: {
+				width: 800,
+				height: 600
+			},
+			gravity: {
+				y:0
+			}
+		}
+	}
 };
+
 
 let game = new Phaser.Game(config);
 
@@ -15,19 +29,66 @@ gameScene.preload = function(){
 
 	//this.load.image('tile0','assets/tile0.png');
 	//this.load.image('grass','assets/grass.png');
+	
 
-	this.load.image('Overworld', 'layout/map/Overworld.png');
+
+	this.load.image('Overworld', 'layout/map2/Overworld.png');
+	this.load.tilemapTiledJSON('layout','layout/map2/datamap.json');
+
+
+
+
+	this.load.image('warrior'
+		,'assets/warrior.png');
+
+	this.load.atlas('war','assets/warriorBody.png','assets/warriorBody.json');
+	this.load.json('body','assets/warriorShape.json');
+	//Working
+	/*this.load.image('Overworld', 'layout/map/Overworld.png');
 	this.load.tilemapTiledJSON('layout','layout/map/datamap.json');
-}
+*/}
 
 gameScene.create = function(){
 	//background
-
 	map = this.make.tilemap({key: 'layout'});
 	tileset = map.addTilesetImage('Overworld');
 	var layer = map.createDynamicLayer(0,tileset,0,0);
 	var layer2 = map.createDynamicLayer(1,tileset,0,0);
-	var layer2 = map.createDynamicLayer(2,tileset,0,0);
+	var layer3 = map.createDynamicLayer(2,tileset,0,0);
+
+	layer.setCollisionFromCollisionGroup();
+	layer2.setCollisionFromCollisionGroup();
+	layer3.setCollisionFromCollisionGroup();
+
+	this.matter.world.convertTilemapLayer(layer);
+	this.matter.world.convertTilemapLayer(layer2);
+	this.matter.world.convertTilemapLayer(layer3);
+
+	//For objects y = mapHeight - tilesize - y;
+	var log = map.createFromObjects('obj',26);
+	log[0].setOrigin(0.5,-0.5);
+	console.log(log);
+
+	var shape = this.cache.json.get('body');
+
+	//Main Player
+	this.player = this.matter.add.sprite(150,400, 'war','warrior.png', {shape: shape.warrior});
+	this.player.setScale(0.5,0.5);
+	this.player.speed = 5;
+
+
+	cursor = this.input.keyboard.createCursorKeys();
+}
+	//Working
+/*	map = this.make.tilemap({key: 'layout'});
+	tileset = map.addTilesetImage('Overworld');
+	var layer = map.createDynamicLayer(0,tileset,0,0);
+	var layer2 = map.createDynamicLayer(1,tileset,0,0);
+	var layer2 = map.createDynamicLayer(2,tileset,0,0);*/
+
+
+
+
 	//Get level data from json
 //	this.level = this.cache.json.get('map');
 	
@@ -44,4 +105,25 @@ gameScene.create = function(){
 /*	map = this.add.tilemap('layout');	
 	tileset = this.map.addTilesetImage('layout/ground_tiles.png','gameTiles');
 	backyardlayer = this.map.createLayer('backyard',tileset)*/
+
+	gameScene.update = function(){
+
+	//Player movement update
+	if (cursor.left.isDown){
+		this.player.x -= (this.player.speed);
+		//this.player.x -= this.playerspeed;
+	}
+	else if (cursor.right.isDown){
+		this.player.x += (this.player.speed);
+		//this.player.x += this.playerspeed;
+	}
+
+	if (cursor.up.isDown){
+		this.player.y -= (this.player.speed);
+		//this.player.y -= this.playerspeed;
+	}
+	else if (cursor.down.isDown){
+		//this.player.y += this.playerspeed;
+		this.player.y += (this.player.speed);
+	}
 }
