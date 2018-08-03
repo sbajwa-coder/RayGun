d0d10ee85d6f1fc5ec61bfd3e341b024c7752546
Shayan Ghazi <shayan.ghazi@mail.utoronto.ca>
2018-08-03 15:42:49 -0400

    title-ing

---
+++ title/js/Example.js
@@ -0,0 +1,16 @@
+class Example extends Phaser.Scene {
+	constructor() {
+		super({
+			key: "Example"});
+	}
+
+	preload() {
+	}
+
+	create() {
+		//this.matter.world.setBounds(0, 0, game.config.width, game.config.height);
+	}
+
+	update(delta) {
+	}
+}
---
+++ title/js/game.js
@@ -0,0 +1,18 @@
+var config = {
+	type:Phaser.AUTO,
+	width:800,
+	height:600,
+	'phaser-example',
+	//backgroundColor:'#ffffff',
+	transparent:true,
+	physics: {
+		default: 'matter',
+		matter: {
+			gravity: {y : 0, x: 0},
+			debug: true
+		}
+	},
+	scene: [Example]
+};
+
+var game = new Phaser.Game(config);
