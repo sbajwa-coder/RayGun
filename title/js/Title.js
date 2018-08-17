28d9bc25a7e4e1ea8770d8482c47222361e0d9e5
Shayan Ghazi <shayan.ghazi@mail.utoronto.ca>
2018-08-17 09:22:39 -0400

    added disable buttons and more styling + markup

--- title/js/Title.js
+++ title/js/Title.js
@@ -17,10 +17,27 @@ class Title extends Phaser.Scene {
 		// playButton.setDisplayOrigin(112,50);
 		// Phaser.Display.Align.In.Center(bg, this.add.zone(400, 300, 800, 600));
 		// Phaser.Display.Align.In.Center(playButton, bg);
-
-	    $("#shockfort-game").load('../html/entry-screen.html');
+		
+		$("#shockfort-game").load('../html/entry-screen.html');
+		// $("body").click(function(event) {
+		// 	 Act on the event 
+		// 	alert($(this));
+		// });
+		// $(".button").hover(function() {
+		// 	/* Stuff to do when the mouse enters the element */
+		// 	alert("trynna login boi?");
+		// 	const currentWidth = $(this).css("width");
+		// 	$(this).css("width", currentWidth + 10);
+		// }, function() {
+		// 	/* Stuff to do when the mouse leaves the element */
+		// });
+		$(".modal").click(function(event) {
+			/* Act on the event */
+			alert("hiiii");
+		});
 	}
 
 	update(delta) {
+
 	}
 }
