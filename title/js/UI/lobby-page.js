2f54ef68d4db41b6336fbe84cab833bc5d03913e
Shayan Ghazi <shayan.ghazi@mail.utoronto.ca>
2018-08-28 16:04:50 -0400

    pre master merge

--- title/js/UI/lobby-page.js
+++ title/js/UI/lobby-page.js
@@ -7,6 +7,243 @@
 
 //$(".modal-screen").css({"display": "block","opacity": "0.5"});
 
+// CREATE GAME INTERACTIONS ===============
+function TextCarousel(values, back, forward, field, idx=0) {
+	this.back = back;
+	this.forward = forward;
+	this.values = values;
+	this.field = field;
+	this.idx = idx;
+
+	this.assignValue = function() {
+		this.field.html(this.values[this.idx]);
+	}
+
+	this.wireArrow = function() {
+		backClick = function(event) {
+			this.idx -= 1;
+			if (this.idx < 0) {
+				this.idx = this.values.length - 1;
+			}
+			this.assignValue();
+		}
+
+		forwardClick = function(event) {
+			this.idx += 1;
+			if (this.idx >= this.values.length) {
+				this.idx = 0;
+			}
+			this.assignValue();
+		}
+		this.back.click(backClick.bind(this));
+		this.forward.click(forwardClick.bind(this));
+	}
+
+	this.wireValue = function() {
+
+		minusClick = function(event) {
+			if (this.idx == this.values.length-1) {
+				this.forward.css('background-image', 'url(../../assets/UI/plus-button.png)');
+			}
+			if(this.idx >= 1) {
+				this.idx -= 1;
+				this.assignValue();
+				if (this.idx == 0) {
+					this.back.css('background-image', 'url(../../assets/UI/minus-button-disabled.png)');
+				}
+			}
+		}
+
+		plusClick = function(event) {
+			if (this.idx == 0) {
+				this.back.css('background-image', 'url(../../assets/UI/minus-button.png)');
+			}
+			if (this.idx < this.values.length-1) {
+				this.idx += 1;
+				this.assignValue();
+				if (this.idx == this.values.length-1) {
+					this.forward.css('background-image', 'url(../../assets/UI/plus-button-disabled.png)');
+				}
+			}
+		}
+		this.back.click(minusClick.bind(this));
+		this.forward.click(plusClick.bind(this));
+	}
+}
+
+// game mode carousel
+var gameModes = ["FFA", "CTF", "TDM"];
+var gameModeCarousel = new TextCarousel(gameModes, $("#gameModeCarousel .carousel-back"),
+					$("#gameModeCarousel .carousel-forward"),
+					$("#gameModeValue"));
+gameModeCarousel.wireArrow();
+
+// capacity carousel
+var minCapacity = 2;
+var maxCapacity = 10;
+var capacities = [...Array(maxCapacity+1).keys()].slice(minCapacity, maxCapacity+1);
+
+var capacityCarousel = new TextCarousel(capacities, $("#capacityCarousel .carousel-back"),
+					$("#capacityCarousel .carousel-forward"),
+					$("#capacityValue"), 5);
+capacityCarousel.wireArrow();
+
+// rank carousel
+var ranks = ["Recruit", "Lieutenant", "Captain", "General"];
+
+var rankCarousel = new TextCarousel(ranks, $("#rankCarousel .minus-button"),
+					$("#rankCarousel .plus-button"), $("#rankValue"));
+rankCarousel.wireValue();
+
+// RADIO BUTTON SYSTEM -------
+function RadioButtonSystem(buttons, current=null) {
+	this.buttons = buttons;
+	if (current == null) this.current = buttons[0];
+	else this.current = current;
+
+	this.wire = function() {
+		for (button of this.buttons) {
+			radioClick = function(btn) {
+				/* Act on the event */
+				let img1 = $(this.current).children('img');
+				img1.attr('src','../assets/UI/radio-button-off.png');
+
+				let img2 = $(btn).children("img");
+				img2.attr('src','../assets/UI/radio-button-on.png');
+				this.current = btn;
+			}
+			binded = radioClick.bind(this);
+			$(button).click(function() {
+
+				binded($(this));
+			});
+		}
+	}
+}
+
+var passwordRadioButtons = $("#passwordRadio > .radio-item");
+var passwordRadio = new RadioButtonSystem(passwordRadioButtons);
+passwordRadio.wire();
+
+$("#noPassword").click(function(event) {
+	/* Act on the event */ 
+	$("#gamePassword").val("").addClass('disabled-input');
+});
+
+$("#yesPassword").click(function(event) {
+	/* Act on the event */
+	$("#gamePassword").removeClass('disabled-input');
+});
+
+
+// MAP CAROUSEL =========================
+function ImageCarousel(images, back, forward, field, idx=0) {
+	this.tc = new TextCarousel(images, back, forward, field, idx);
+	this.back = this.tc.back;
+	this.forward = this.tc.forward;
+	this.images = images;
+	this.values = images;
+	this.field = this.tc.field;
+	this.idx = this.tc.idx;
+
+	this.assignValue = function() {
+		const img = "url(../assets/UI/"+this.images[this.idx]+".png)";
+		this.field.css("background-image", img);
+	}
+
+	this.wireArrow = this.tc.wireArrow;
+}
+
+var maps = ["SHOCKFORT", "PURPLE", "MAGICAL"];
+var mapCarouselNames = new TextCarousel(maps,
+$(".map-selection > .carousel-back"),
+$(".map-selection > .carousel-forward"),
+$("#mapSelectionName"));
+mapCarouselNames.wireArrow();
+
+var mapImages = ["shockfort-bg", "purple-sky", "magical-sky"];
+var mapCarouselImages = new ImageCarousel(mapImages,
+$(".map-selection > .carousel-back"),
+$(".map-selection > .carousel-forward"),
+$("#mapSelection"));
+mapCarouselImages.wireArrow();
+
+var mapCaptions = ["caption 1", "caption 2", "caption 3"];
+var mapCarouselCaptions = new TextCarousel(mapCaptions,
+$(".map-selection > .carousel-back"),
+$(".map-selection > .carousel-forward"),
+$("#mapCaption"));
+mapCarouselCaptions.wireArrow();
+
+
+// CREATE / CANCEL RESPONDERS ===========
+$("#createCancel").click(function(event) {
+	/* Act on the event */
+	$(".modal-screen").trigger('click');
+});
+
+$("#createConfirm").click(function(event) {
+	/* Act on the event */
+	console.log('confirm');
+});
+
+// $("#gameModeCarousel .carousel-back").click(function(event) {
+// 	/* Act on the event */
+
+// });
+
+// $("#gameModeCarousel .carousel-forward").click(function(event) {
+// 	/* Act on the event */
+	
+// });
+
+
+// PLAYER PREVIEW =====================
+var characters = [
+	{
+		name: "design1",
+		hp: 300,
+		mp: 90
+	},
+	{
+		name: "design2",
+		hp: 400,
+		mp: 100
+	},
+	{
+		name: "design3",
+		hp: 9999,
+		mp: 9999
+	}
+];
+var previewIdx = 0;
+
+function assignPreviewInfo(idx) {
+	const img = "url(../../assets/"+characters[idx].name+".png)";
+	$(".player-preview").css("background-image", img);
+	$(".player-preview > .character-name").html(characters[idx].name);
+	$("#hp").html(characters[idx].hp);
+	$("#mp").html(characters[idx].mp);
+}
+
+$(".player-preview > .carousel-back").click(function(event) {
+	/* Act on the event */
+	previewIdx -= 1;
+	if (previewIdx < 0) {
+		previewIdx = characters.length -1;
+	}
+	assignPreviewInfo(previewIdx);
+});
+
+$(".player-preview > .carousel-forward").click(function(event) {
+	/* Act on the event */
+	previewIdx += 1;
+	if (previewIdx >= characters.length) {
+		previewIdx = 0;
+	}
+	assignPreviewInfo(previewIdx);
+});
+
 // OFF-CLICK ========================
 $(document).click(function(event) {
 	/* Act on the event */
